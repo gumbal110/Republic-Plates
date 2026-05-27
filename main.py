5033fc699484b4b24020c6d0077d1d490e2f81bf
GumballDev <yenselmanuel0@gmail.com>
2026-05-27 15:35:50 -0400

    Todo relizado exitozamente

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import time
+
 def main():
     print("Hello from repl-nix-workspace!")
-
-
+    while True:
+        # tu lógica aquí
+        time.sleep(60)  # hace algo cada 60 segundos
+        
 if __name__ == "__main__":
     main()
